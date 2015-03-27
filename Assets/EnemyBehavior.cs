fe2d587e92d961d22b50e7139484ffdaef8d9956
Andrew Clark <andrew@enlivenhq.com>
2015-03-26 23:29:07 -0500

    Added 'Enemies' that get generated around the player and move towards the player until the player looks at the enemy.

---
+++ Assets/EnemyBehavior.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    public bool alive = true;
+    bool visible;
+    bool trackTarget;
+    Material cubeMaterial;
+    Color color;
+    int moveSpeed = 8;
+    int rotationSpeed = 3;
+    float startTime;
+    float randomValue;
+    Transform target;
+    Transform myTransform;
+    float deathTimer;
+
+    void Awake()
+    {
+        myTransform = transform;
+    }
+
+    void Start()
+    {
+        visible = false;
+        trackTarget = false;
+        //audio = GetComponent<TBE_3DCore.TBE_Source>();
+        startTime = Time.time;
+        MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        cubeMaterial = (meshRenderer.materials[0]);
+        color = cubeMaterial.color;
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(visible){
+            deathTimer += Time.deltaTime;
+            if (deathTimer >= 3)
+            {
+                alive = false;
+            }
+        }
+        
+        randomValue = Random.value;
+        if (randomValue > .5 && randomValue < .51 && !trackTarget && !visible)
+        {
+            trackTarget = true;
+            startTime = Time.time;
+        }
+        if (trackTarget && FiveSecondsHavePassed())
+        {
+            TrackTarget();
+        }
+        if (visible)
+        {
+            color = Color.Lerp(color, Color.red, 0.01f);
+            cubeMaterial.SetColor("_Color", color);
+        }
+        else
+        {
+            color = Color.Lerp(color, Color.white, 0.01f);
+            cubeMaterial.SetColor("_Color", color);
+        }
+    }
+
+    void OnBecameVisible()
+    {
+        visible = true;
+        trackTarget = false;
+        deathTimer = 0;
+    }
+
+    void OnBecameInvisible()
+    {
+        visible = false;
+        deathTimer = 0;
+    }
+
+    void TrackTarget()
+    {
+        myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+        Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+
+        myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+    }
+
+    bool FiveSecondsHavePassed()
+    {
+        return (Time.time - startTime) > 0;
+    }
+}
---
+++ Assets/EnemyGenerator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyGenerator : MonoBehaviour {
+
+    public GameObject[] enemies;
+    private int CurrentEnemy;
+    private bool holdEnemyGeneration = false;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (enemies.Length > CurrentEnemy || holdEnemyGeneration)
+        {
+            var currentEnemy = enemies[CurrentEnemy].GetComponent<EnemyBehavior>();
+            if (!currentEnemy.alive)
+            {
+                enemies[CurrentEnemy].SetActive(false);
+                CurrentEnemy++;
+                if (enemies.Length > CurrentEnemy)
+                {
+                    holdEnemyGeneration = true;
+                    var random = Random.Range(.5f, 5f);
+                    Invoke("ActivateNextEnemy", random);
+                }
+            }
+        }
+	}
+
+    void ActivateNextEnemy()
+    {
+        holdEnemyGeneration = false;
+        enemies[CurrentEnemy].SetActive(true);
+    }
+}
