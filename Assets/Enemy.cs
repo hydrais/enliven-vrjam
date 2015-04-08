35f95def56fa4049f2ad849a298f4bd7d42f34e8
Jesse Dunlap <jesse@hydrais.com>
2015-04-07 22:19:09 -0500

    Pathfinding test.

---
+++ Assets/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	public GameObject Player;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		var playerPosition = Player.transform.position;
+		this.transform.position = playerPosition + new Vector3 (0, 5, 5);
+	}
+}
---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+	public GameObject Player;
+
+	void Update () {
+		NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent> ();
+		navMeshAgent.destination = Player.transform.position;
+	}
+}
