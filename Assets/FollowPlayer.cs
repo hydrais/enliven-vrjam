2b12f422ebbb9915b1a8c3ffd115f4718c73b481
Andrew Clark <andrew@enlivenhq.com>
2015-04-18 19:31:12 -0500

    testing out death animations instead of completely removing the enemies.

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class FollowPlayer : MonoBehaviour {
 	public GameObject Player;
+    private NavMeshAgent EnemyMeshAgent;
+    private bool dead = false;
 
+    void Start()
+    {
+        EnemyMeshAgent = GetComponent<NavMeshAgent>();
+    }
 	void Update () {
-		NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent> ();
-		navMeshAgent.destination = Player.transform.position;
+        if (!dead)
+        {
+            EnemyMeshAgent.destination = Player.transform.position;
+        }
+        else
+        {
+            EnemyMeshAgent.Stop();
+        }
 	}
 
 	public void Kill () {
-		Destroy(this.gameObject);
+        dead = true;
+        GetComponent<Animator>().Play("dead");
+        GetComponent<AudioSource>().Stop();
+		//Destroy(this.gameObject);
 	}
 }
